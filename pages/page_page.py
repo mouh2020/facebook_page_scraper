3f9d1060740c992719bbd8e3bf5376c3fbfd10ef
mouh2020 <badimohammed2019@gmail.com>
2023-07-07 23:03:06 +0100

    setup pages

---
+++ pages/page_page.py
@@ -0,0 +1,13 @@
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+class Page:
+    pass
+
+
+
+
+
+
---
+++ pages/post_page.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+class Post : 
+    pass
